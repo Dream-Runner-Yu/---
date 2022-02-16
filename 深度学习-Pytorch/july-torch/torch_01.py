cf51c5ee5bddfa7825babedc6aee1bbaac2fd342
EMM <yuyuyuyuyu1234@users.noreply.github.com>
2022-02-16 23:12:30 +0800

    日常代码记录

---
+++ 深度学习-Pytorch/july-torch/torch_01.py
@@ -0,0 +1,35 @@
+import torch
+
+a = torch.empty(5,3)
+
+x = torch.rand(5,3)
+
+zero = torch.zeros(5,3,dtype=torch.long)
+
+# x.types
+
+x = torch.tensor([5,5,3])
+
+x = x.new_ones(5,3,dtype=torch.double)
+
+torch.rand_like(x,dtype=torch.float)
+
+y = torch.rand(5,3)
+
+x + y
+
+torch.add(x,y)
+
+out = torch.empty(5,3)
+
+# y += x
+y.add_(x)
+
+
+
+
+
+
+
+
+
